7eb1fbe5f0411c4b3cc94cc9e5c2683d22578a20
Kai <20014063@myrp.edu.sg>
2021-04-28 15:25:38 +0800

    Added new comment

---
+++ app/src/main/java/sg/edu/rp/c346/id20014063/demomysecondapp/Worksheet2E1a.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id20014063.demomysecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Worksheet2E1a extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_worksheet2_e1a);
+    }
+}
